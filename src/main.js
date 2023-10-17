41db81e8540db8c4b94b28fe04e4d75009daf293
MeenaAlagiah <117711147+MeenaAlagiah@users.noreply.github.com>
2023-10-17 13:02:25 +0530

    Committing the Vue Dropdown Menu Getting Started sample

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { registerLicense } from '@syncfusion/ej2-base';
+registerLicense("ORg4AjUWIQA/Gnt2VlhhQlJCfV5DQmtWfFN0RnNYdV53flFDcDwsT3RfQF5iSX5ad0ZjUX1fdXFXTw==");
+createApp(App).mount('#app')
